395e8b06a303be122315876a464549d21bfe1688
Léo Mendes <113518053+LeoMendesEsEtml@users.noreply.github.com>
2025-03-11 23:14:21 +0100

    Latest

--- TP3_MenuGen/firmware/src/MenuGen.c
+++ TP3_MenuGen/firmware/src/MenuGen.c
@@ -49,276 +49,238 @@ void MENU_Display(S_ParamGen *pParam, uint8_t selection)
 
 void MENU_Execute(S_ParamGen *pParam) {
     static MenuState_t menu = MENU_FORME_SEL;
-    static int8_t minValues[4] = {1,1,1,1};
-    static int8_t maxValues[4] = {10,10,10,10};    
+    static uint8_t Timer_S9 = 0; // Timer pour la détection de l'appui long sur S9
+    const char MenuFormes[4][21] = {"Sinus", "Triangle", "DentDeScie", "Carre"};
+
     switch (menu) {
         case MENU_FORME_SEL:
-            if (Pec12IsPlus() == true) {
+            if (Pec12IsPlus()) {
                 menu = MENU_FREQ_SEL;
                 ClearLcd();
                 Pec12ClearPlus();
             }
-            if (Pec12IsMinus() == true) {
+            if (Pec12IsMinus()) {
                 menu = MENU_OFFSET_SEL;
                 ClearLcd();
                 Pec12ClearMinus();
             }
-            if (Pec12IsOK() == true) {
+            if (Pec12IsOK()) {  
                 menu = MENU_FORME_EDIT;
                 ClearLcd();
                 Pec12ClearOK();
             }
+            if (S9IsOK() && Timer_S9 >= 200) {  
+                menu = MENU_FORME_EDIT;
+                Timer_S9 = 0;
+                ClearLcd();
+                S9ClearOK();
+            }
             MENU_Display(pParam, 1);
             lcd_gotoxy(1, 1);
             printf_lcd("*");
             break;
-            
+
         case MENU_FORME_EDIT:
-            if (Pec12IsPlus() == true) {
-                pParam->Forme++;
-                if (pParam->Forme > maxValues[0]) {
-                    pParam->Forme = minValues[0];
-                }
+            if (Pec12IsPlus()) {
+                pParam->Forme = (pParam->Forme + 1) % 4;
                 Pec12ClearPlus();
                 ClearLcd();
             }
-            if (Pec12IsMinus() == true) {
-                menu = MENU_OFFSET_SEL;
-                pParam->Forme--;
-                if (pParam->Forme < minValues[0]) {
-                    pParam->Forme = maxValues[0];
-                }
+            if (Pec12IsMinus()) {
+                pParam->Forme = (pParam->Forme - 1 + 4) % 4;
                 Pec12ClearMinus();
+                ClearLcd();
+            }
+            if (Pec12IsOK()) {  
+                menu = MENU_FORME_SEL;
+                Pec12ClearOK();
+                ClearLcd();
+            }
+            if (S9IsOK()) {  
+                menu = MENU_FORME_SEL;
+                pParam->Forme = NVM_ReadParam()->Forme;
+                ClearLcd();
+                S9ClearOK();
             }
             MENU_Display(pParam, 1);
             lcd_gotoxy(1, 1);
-            printf_lcd("?");                        
+            printf_lcd("?");
             break;
-            
+
         case MENU_FREQ_SEL:
-            if (Pec12IsPlus() == true) {
-                menu = MENU_FORME_SEL;
+            if (Pec12IsPlus()) {
+                menu = MENU_AMPL_SEL;
                 ClearLcd();
                 Pec12ClearPlus();
             }
-            if (Pec12IsMinus() == true) {
-                menu = MENU_AMPL_SEL;
+            if (Pec12IsMinus()) {
+                menu = MENU_FORME_SEL;
                 ClearLcd();
                 Pec12ClearMinus();
             }
-            if (Pec12IsOK() == true) {
+            if (Pec12IsOK()) {
                 menu = MENU_FREQ_EDIT;
                 ClearLcd();
                 Pec12ClearOK();
             }
-            MENU_Display(pParam, 1);
+            MENU_Display(pParam, 2);
             lcd_gotoxy(1, 2);
             printf_lcd("*");
             break;
+
         case MENU_FREQ_EDIT:
-            if (Pec12IsPlus() == true) {
-                pParam->Frequence++;
-                if (pParam->Frequence > maxValues[1]) {
-                    pParam->Frequence = minValues[1];
-                }
+            if (Pec12IsPlus()) {
+                pParam->Frequence += 10;
+                if (pParam->Frequence > 2000) pParam->Frequence = 20;
                 Pec12ClearPlus();
                 ClearLcd();
             }
-            if (Pec12IsMinus() == true) {
-                menu = MENU_OFFSET_SEL;
-                pParam->Frequence--;
-                if (pParam->Frequence < minValues[1]) {
-                    pParam->Frequence = maxValues[1];
-                }
+            if (Pec12IsMinus()) {
+                pParam->Frequence -= 10;
+                if (pParam->Frequence < 20) pParam->Frequence = 2000;
                 Pec12ClearMinus();
                 ClearLcd();
             }
-            if (Pec12IsOK() == true) {
-                menu = MENU_FREQ_EDIT;
-                ClearLcd();
+            if (Pec12IsOK()) {
+                menu = MENU_FREQ_SEL;
                 Pec12ClearOK();
+                ClearLcd();
             }
-            MENU_Display(pParam, 1);
+            if (S9IsOK()) {
+                menu = MENU_FREQ_SEL;
+                pParam->Frequence = NVM_ReadParam()->Frequence;
+                ClearLcd();
+                S9ClearOK();
+            }
+            MENU_Display(pParam, 2);
             lcd_gotoxy(1, 2);
             printf_lcd("?");
             break;
+
         case MENU_AMPL_SEL:
-            if (Pec12IsPlus() == true) {
+            if (Pec12IsPlus()) {
                 menu = MENU_OFFSET_SEL;
                 ClearLcd();
                 Pec12ClearPlus();
             }
-            if (Pec12IsMinus() == true) {
+            if (Pec12IsMinus()) {
+                menu = MENU_FREQ_SEL;
+                ClearLcd();
                 Pec12ClearMinus();
-
             }
-            if (Pec12IsOK() == true) {
+            if (Pec12IsOK()) {
                 menu = MENU_AMPL_EDIT;
                 ClearLcd();
                 Pec12ClearOK();
             }
-            MENU_Display(pParam, 1);
+            MENU_Display(pParam, 3);
             lcd_gotoxy(1, 3);
             printf_lcd("*");
             break;
 
         case MENU_AMPL_EDIT:
-            if (Pec12IsPlus() == true) {
-                pParam->Amplitude++;
-                if (pParam->Amplitude > maxValues[2]) {
-                    pParam->Amplitude = minValues[2];
-                }
+            if (Pec12IsPlus()) {
+                pParam->Amplitude += 100;
+                if (pParam->Amplitude > 10000) pParam->Amplitude = 0;
                 Pec12ClearPlus();
                 ClearLcd();
             }
-            if (Pec12IsMinus() == true) {
-                menu = MENU_OFFSET_SEL;
-                pParam->Forme--;
-                if (pParam->Forme < minValues[2]) {
-                    pParam->Forme = maxValues[2];
-                }
+            if (Pec12IsMinus()) {
+                pParam->Amplitude -= 100;
+                if (pParam->Amplitude < 0) pParam->Amplitude = 10000;
                 Pec12ClearMinus();
+                ClearLcd();
             }
-            MENU_Display(pParam, 1);
+            if (Pec12IsOK()) {
+                menu = MENU_AMPL_SEL;
+                Pec12ClearOK();
+                ClearLcd();
+            }
+            if (S9IsOK()) {
+                menu = MENU_AMPL_SEL;
+                pParam->Amplitude = NVM_ReadParam()->Amplitude;
+                ClearLcd();
+                S9ClearOK();
+            }
+            MENU_Display(pParam, 3);
             lcd_gotoxy(1, 3);
             printf_lcd("?");
             break;
+
         case MENU_OFFSET_SEL:
-            if (Pec12IsPlus() == true) {
-                menu = MENU_OFFSET_SEL;
+            if (Pec12IsPlus()) {
+                menu = MENU_FORME_SEL;
                 ClearLcd();
                 Pec12ClearPlus();
             }
-            if (Pec12IsMinus() == true) {
-                menu = MENU_FREQ_SEL;
+            if (Pec12IsMinus()) {
+                menu = MENU_AMPL_SEL;
                 ClearLcd();
                 Pec12ClearMinus();
             }
-            if (Pec12IsOK() == true) {
-                menu = MENU_AMPL_EDIT;
+            if (Pec12IsOK()) {
+                menu = MENU_OFFSET_EDIT;
                 ClearLcd();
                 Pec12ClearOK();
             }
-            MENU_Display(pParam, 1);
+            MENU_Display(pParam, 4);
             lcd_gotoxy(1, 4);
             printf_lcd("*");
             break;
 
         case MENU_OFFSET_EDIT:
-            if (Pec12IsPlus() == true) {
-                pParam->Offset++;
-                if (pParam->Offset > maxValues[2]) {
-                    pParam->Offset = minValues[2];
-                }
+            if (Pec12IsPlus()) {
+                pParam->Offset += 100;
+                if (pParam->Offset > 5000) pParam->Offset = -5000;
                 Pec12ClearPlus();
                 ClearLcd();
             }
-            if (Pec12IsMinus() == true) {
-                menu = MENU_OFFSET_SEL;
-                pParam->Offset--;
-                if (pParam->Offset < minValues[3]) {
-                    pParam->Offset = maxValues[3];
-                }
+            if (Pec12IsMinus()) {
+                pParam->Offset -= 100;
+                if (pParam->Offset < -5000) pParam->Offset = 5000;
                 Pec12ClearMinus();
-            }            
-            MENU_Display(pParam, 1);
+                ClearLcd();
+            }
+            if (Pec12IsOK()) {
+                menu = MENU_OFFSET_SEL;
+                Pec12ClearOK();
+                ClearLcd();
+            }
+            if (S9IsOK()) {
+                menu = MENU_OFFSET_SEL;
+                pParam->Offset = NVM_ReadParam()->Offset;
+                ClearLcd();
+                S9ClearOK();
+            }
+            MENU_Display(pParam, 4);
             lcd_gotoxy(1, 4);
             printf_lcd("?");
             break;
 
-        default:
-            
+        case MENU_SAUVEGARDE:
+            if (S9IsOK()) {
+                menu = MENU_FORME_SEL;
+                Timer_S9 = 0;
+                ClearLcd();
+            }
+            if (Pec12IsOK()) {
+                NVM_WriteBlock((uint32_t *) pParam, sizeof(S_ParamGen));
+                lcd_gotoxy(6, 2);
+                printf_lcd("Sauvegarde OK !");
+                menu = MENU_FORME_SEL;
+                Timer_S9 = 0;
+            }
             break;
     }
-    
-    /* if (editing == false) {
-        if (Pec12IsPlus() == true) {
-            Pec12ClearPlus();
-            selection++;
-            if (selection >= 4) {
-                selection = 1;
-            }
-            MENU_Display(pParam,selection);
-        }
-        if (Pec12IsMinus() == true) {
-            Pec12ClearMinus();
-            selection--;
-            if (selection < 1) {
-                selection = 3;
-            }
-            MENU_Display(pParam,selection);
-        }
-        if (Pec12IsOK() == true) {
-            Pec12ClearOK();
-            editing = true;
-            MENU_Display(pParam,selection);
-        }
-    } else {
-        if (Pec12IsPlus() == true) {
-            Pec12ClearPlus();
-            if (selection == 0) {
 
-            } else if (selection == 1) {
-                pParam->Frequence = pParam->Frequence + stepValues[1];
-                if (pParam->Frequence > maxValues[1]) {
-                    pParam->Frequence = minValues[1];
-                }
-            } else if (selection == 2) {
-                pParam->Amplitude = pParam->Amplitude + stepValues[2];
-                if (pParam->Amplitude > maxValues[2]) {
-                    pParam->Amplitude = minValues[2];
-                }
-            } else if (selection == 3) {
-                pParam->Offset = pParam->Offset + stepValues[3];
-                if (pParam->Offset > maxValues[3]) {
-                    pParam->Offset = minValues[3];
-                }
-            }
-            MENU_Display(pParam, selection);
-        }
-        if (Pec12IsMinus() == true) {
-            Pec12ClearMinus();
-            if (selection == 0) {
-                pParam->Forme = pParam->Forme - 1;
-                if (pParam->Forme < minValues[0]) {
-                    pParam->Forme = maxValues[0];
-                }
-            } else if (selection == 1) {
-                pParam->Frequence = pParam->Frequence - stepValues[1];
-                if (pParam->Frequence < minValues[1]) {
-                    pParam->Frequence = maxValues[1];
-                }
-            } else if (selection == 2) {
-                pParam->Amplitude = pParam->Amplitude - stepValues[2];
-                if (pParam->Amplitude < minValues[2]) {
-                    pParam->Amplitude = maxValues[2];
-                }
-            } else if (selection == 3) {
-                pParam->Offset = pParam->Offset - stepValues[3];
-                if (pParam->Offset < minValues[3]) {
-                    pParam->Offset = maxValues[3];
-                }
-            }
-        }
-        if (Pec12IsOK() == true) {
-            Pec12ClearOK();
-            editing = false;
-        }
-        if (Pec12IsESC() == true) {
-            Pec12ClearESC();
-            editing = false;
-        }
-        if (S9IsOK() == true) {
-            S9ClearOK();
-            NVM_WriteBlock((uint32_t *) pParam, sizeof (S_ParamGen));
-            lcd_gotoxy(2, 4);
-            printf_lcd("Sauvegarde OK !");
-        } else {
-            MENU_Display(pParam, selection);
-        }
-    }
-     */
+    Pec12ClearInactivity();
+    Pec12ClearPlus();
+    Pec12ClearMinus();
+    Pec12ClearOK();
+    Pec12ClearESC();
+    S9ClearOK();
 }
+
 
 
 
