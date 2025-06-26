442661fde43ca8b9733dead7a85a88729c998152
Léo Mendes <113518053+LeoMendesEsEtml@users.noreply.github.com>
2025-06-26 10:05:37 +0200

    latest

--- tcpip_tcp_servert_TP5_IpGen/firmware/tcpip_tcp_serve_TP5_IPGEN_.X/nbproject/Makefile-variables.mk
+++ tcpip_tcp_servert_TP5_IpGen/firmware/tcpip_tcp_serve_TP5_IPGEN_.X/nbproject/Makefile-variables.mk
@@ -8,3 +8,6 @@ CND_BASEDIR=`pwd`
 CND_ARTIFACT_DIR_pic32mx_eth_sk2=dist/pic32mx_eth_sk2/production
 CND_ARTIFACT_NAME_pic32mx_eth_sk2=tcpip_tcp_serve_TP5_IPGEN_.X.production.hex
 CND_ARTIFACT_PATH_pic32mx_eth_sk2=dist/pic32mx_eth_sk2/production/tcpip_tcp_serve_TP5_IPGEN_.X.production.hex
+CND_PACKAGE_DIR_pic32mx_eth_sk2=${CND_DISTDIR}/pic32mx_eth_sk2/package
+CND_PACKAGE_NAME_pic32mx_eth_sk2=tcpiptcpservetp5ipgen.x.tar
+CND_PACKAGE_PATH_pic32mx_eth_sk2=${CND_DISTDIR}/pic32mx_eth_sk2/package/tcpiptcpservetp5ipgen.x.tar
